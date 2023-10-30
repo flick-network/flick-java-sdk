96aa09851b532a368a7fd0912968465bae43bd65
Sivadas <sivadasrajan@gmail.com>
2023-10-31 03:19:08 +0530

    All testcases done

--- src/main/java/com/flick/sdk/types/InvoiceData.java
+++ src/main/java/com/flick/sdk/types/InvoiceData.java
@@ -1,5 +1,6 @@
 package com.flick.sdk.types;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InvoiceData {
@@ -14,55 +15,408 @@ public class InvoiceData {
     public String inv_type;
     public int payment_method;
     public String currency;
-    public String total_tax;
+    public Double total_tax;
     public List<LineItem> lineitems;
-}
 
-class PartyDetails {
-    public String party_name_ar;
-    public String party_name_en;
-    public String party_vat;
-    public PartyAddId party_add_id;
-    public String city_ar;
-    public String city_en;
-    public String city_subdivision_ar;
-    public String city_subdivision_en;
-    public String street_ar;
-    public String street_en;
-    public String plot_identification;
-    public String building;
-    public String postal_zone;
-}
+    public String getEgsUuid() {
+        return egs_uuid;
+    }
 
-class PartyAddId {
-    public int crn;
-}
+    public void setEgsUuid(String egs_uuid) {
+        this.egs_uuid = egs_uuid;
+    }
 
-class AdvanceDetails {
-    public double advance_amount;
-    public double total_amount;
-    public List<AdvanceInvoice> advance_invoices;
-}
+    public String getInvoiceRefNumber() {
+        return invoice_ref_number;
+    }
 
-class AdvanceInvoice {
-    public String tax_category;
-    public double tax_percentage;
-    public double taxable_amount;
-    public double tax_amount;
-    public List<Invoice> invoices;
-}
+    public void setInvoiceRefNumber(String invoice_ref_number) {
+        this.invoice_ref_number = invoice_ref_number;
+    }
 
-class Invoice {
-    public String id;
-    public String issue_date;
-    public String issue_time;
-}
+    public String getIssueDate() {
+        return issue_date;
+    }
 
-class LineItem {
-    public String name_ar;
-    public String name_en;
-    public double quantity;
-    public String tax_category;
-    public double tax_exclusive_price;
-    public double tax_percentage;
+    public void setIssueDate(String issue_date) {
+        this.issue_date = issue_date;
+    }
+
+    public String getIssueTime() {
+        return issue_time;
+    }
+
+    public void setIssueTime(String issue_time) {
+        this.issue_time = issue_time;
+    }
+
+    public PartyDetails getPartyDetails() {
+        return party_details;
+    }
+
+    public void setPartyDetails(PartyDetails party_details) {
+        this.party_details = party_details;
+    }
+
+    public String getDocType() {
+        return doc_type;
+    }
+
+    public void setDocType(String doc_type) {
+        this.doc_type = doc_type;
+    }
+
+    public Boolean getHasAdvance() {
+        return has_advance;
+    }
+
+    public void setHasAdvance(Boolean has_advance) {
+        this.has_advance = has_advance;
+    }
+
+    public AdvanceDetails getAdvanceDetails() {
+        return advance_details;
+    }
+
+    public void setAdvanceDetails(AdvanceDetails advance_details) {
+        this.advance_details = advance_details;
+    }
+
+    public String getInvType() {
+        return inv_type;
+    }
+
+    public void setInvType(String inv_type) {
+        this.inv_type = inv_type;
+    }
+
+    public int getPaymentMethod() {
+        return payment_method;
+    }
+
+    public void setPaymentMethod(int payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Double getTotalTax() {
+        return total_tax;
+    }
+
+    public void setTotalTax(Double total_tax) {
+        this.total_tax = total_tax;
+    }
+
+    public List<LineItem> getLineItems() {
+        return lineitems;
+    }
+
+    public void setLineItems(List<LineItem> lineitems) {
+        this.lineitems = lineitems;
+    }
+
+    public static class PartyDetails {
+        public String party_name_ar;
+        public String party_name_en;
+        public String party_vat;
+        public PartyAddId party_add_id;
+        public String city_ar;
+        public String city_en;
+        public String city_subdivision_ar;
+        public String city_subdivision_en;
+        public String street_ar;
+        public String street_en;
+        public String plot_identification;
+        public String building;
+        public String postal_zone;
+
+        public String getPartyNameAr() {
+            return party_name_ar;
+        }
+
+        public void setPartyNameAr(String party_name_ar) {
+            this.party_name_ar = party_name_ar;
+        }
+
+        public String getPartyNameEn() {
+            return party_name_en;
+        }
+
+        public void setPartyNameEn(String party_name_en) {
+            this.party_name_en = party_name_en;
+        }
+
+        public String getPartyVat() {
+            return party_vat;
+        }
+
+        public void setPartyVat(String party_vat) {
+            this.party_vat = party_vat;
+        }
+
+        public PartyAddId getPartyAddId() {
+            return party_add_id;
+        }
+
+        public void setPartyAddId(PartyAddId party_add_id) {
+            this.party_add_id = party_add_id;
+        }
+
+        public String getCityAr() {
+            return city_ar;
+        }
+
+        public void setCityAr(String city_ar) {
+            this.city_ar = city_ar;
+        }
+
+        public String getCityEn() {
+            return city_en;
+        }
+
+        public void setCityEn(String city_en) {
+            this.city_en = city_en;
+        }
+
+        public String getCitySubdivisionAr() {
+            return city_subdivision_ar;
+        }
+
+        public void setCitySubdivisionAr(String city_subdivision_ar) {
+            this.city_subdivision_ar = city_subdivision_ar;
+        }
+
+        public String getCitySubdivisionEn() {
+            return city_subdivision_en;
+        }
+
+        public void setCitySubdivisionEn(String city_subdivision_en) {
+            this.city_subdivision_en = city_subdivision_en;
+        }
+
+        public String getStreetAr() {
+            return street_ar;
+        }
+
+        public void setStreetAr(String street_ar) {
+            this.street_ar = street_ar;
+        }
+
+        public String getStreetEn() {
+            return street_en;
+        }
+
+        public void setStreetEn(String street_en) {
+            this.street_en = street_en;
+        }
+
+        public String getPlotIdentification() {
+            return plot_identification;
+        }
+
+        public void setPlotIdentification(String plot_identification) {
+            this.plot_identification = plot_identification;
+        }
+
+        public String getBuilding() {
+            return building;
+        }
+
+        public void setBuilding(String building) {
+            this.building = building;
+        }
+
+        public String getPostalZone() {
+            return postal_zone;
+        }
+
+        public void setPostalZone(String postal_zone) {
+            this.postal_zone = postal_zone;
+        }
+    }
+
+    public static class PartyAddId {
+        public int crn;
+
+        public int getCrn() {
+            return crn;
+        }
+
+        public void setCrn(int crn) {
+            this.crn = crn;
+        }
+    }
+
+    public static class AdvanceDetails {
+        public double advance_amount;
+        public double total_amount;
+        public List<AdvanceInvoice> advance_invoices = new ArrayList<>();;
+
+        public double getAdvanceAmount() {
+            return advance_amount;
+        }
+
+        public void setAdvanceAmount(double advance_amount) {
+            this.advance_amount = advance_amount;
+        }
+
+        public double getTotalAmount() {
+            return total_amount;
+        }
+
+        public void setTotalAmount(double total_amount) {
+            this.total_amount = total_amount;
+        }
+
+        public List<AdvanceInvoice> getAdvanceInvoices() {
+            return advance_invoices;
+        }
+
+        public void setAdvanceInvoices(List<AdvanceInvoice> advance_invoices) {
+            this.advance_invoices = advance_invoices;
+        }
+    }
+
+    public static class AdvanceInvoice {
+        public String tax_category;
+        public double tax_percentage;
+        public double taxable_amount;
+        public double tax_amount;
+        public List<Invoice> invoices = new ArrayList<>();;
+
+        public String getTaxCategory() {
+            return tax_category;
+        }
+
+        public void setTaxCategory(String tax_category) {
+            this.tax_category = tax_category;
+        }
+
+        public double getTaxPercentage() {
+            return tax_percentage;
+        }
+
+        public void setTaxPercentage(double tax_percentage) {
+            this.tax_percentage = tax_percentage;
+        }
+
+        public double getTaxableAmount() {
+            return taxable_amount;
+        }
+
+        public void setTaxableAmount(double taxable_amount) {
+            this.taxable_amount = taxable_amount;
+        }
+
+        public double getTaxAmount() {
+            return tax_amount;
+        }
+
+        public void setTaxAmount(double tax_amount) {
+            this.tax_amount = tax_amount;
+        }
+
+        public List<Invoice> getInvoices() {
+            return invoices;
+        }
+
+        public void setInvoices(List<Invoice> invoices) {
+            this.invoices = invoices;
+        }
+    }
+
+    public static class Invoice {
+        public String id;
+        public String issue_date;
+        public String issue_time;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getIssueDate() {
+            return issue_date;
+        }
+
+        public void setIssueDate(String issue_date) {
+            this.issue_date = issue_date;
+        }
+
+        public String getIssueTime() {
+            return issue_time;
+        }
+
+        public void setIssueTime(String issue_time) {
+            this.issue_time = issue_time;
+        }
+    }
+
+    public static class LineItem {
+        public String name_ar;
+        public String name_en;
+        public double quantity;
+        public String tax_category;
+        public double tax_exclusive_price;
+        public double tax_percentage;
+
+        public String getNameAr() {
+            return name_ar;
+        }
+
+        public void setNameAr(String name_ar) {
+            this.name_ar = name_ar;
+        }
+
+        public String getNameEn() {
+            return name_en;
+        }
+
+        public void setNameEn(String name_en) {
+            this.name_en = name_en;
+        }
+
+        public double getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(double quantity) {
+            this.quantity = quantity;
+        }
+
+        public String getTaxCategory() {
+            return tax_category;
+        }
+
+        public void setTaxCategory(String tax_category) {
+            this.tax_category = tax_category;
+        }
+
+        public double getTaxExclusivePrice() {
+            return tax_exclusive_price;
+        }
+
+        public void setTaxExclusivePrice(double tax_exclusive_price) {
+            this.tax_exclusive_price = tax_exclusive_price;
+        }
+
+        public double getTaxPercentage() {
+            return tax_percentage;
+        }
+
+        public void setTaxPercentage(double tax_percentage) {
+            this.tax_percentage = tax_percentage;
+        }
+    }
+
 }
--- src/test/java/com/flick/sdk/APITest.java
+++ src/test/java/com/flick/sdk/APITest.java
@@ -1,9 +1,19 @@
 package com.flick.sdk;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.containsString;
 
+import com.flick.sdk.types.EGSData;
+import com.flick.sdk.types.EGSData.Device;
+import com.flick.sdk.types.InvoiceData.*;
+import com.flick.sdk.types.InvoiceData;
+import com.google.gson.Gson;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
 
 public class APITest {
 
@@ -11,17 +21,133 @@ public class APITest {
 
     @Test
     public void testOnboardEGS() {
-        assertThat(api.onboardEGS("").toString(), containsString("Hello"));
+
+        EGSData egsData = new EGSData();
+        egsData.setVatName("Test Co.");
+        egsData.setVatNumber("300000000000003");
+
+        // Create Device objects
+        List<Device> devices = new ArrayList<>();
+
+        EGSData.Device device1 = new Device();
+        device1.setDeviceName("TestEGS1");
+        device1.setCity("Riyadh");
+        device1.setCitySubdiv("Test Dist.");
+        device1.setStreet("Test St.");
+        device1.setPlot("1234");
+        device1.setBuilding("1234");
+        device1.setPostal("12345");
+        device1.setBranchName("Riyad Branch 1");
+        device1.setBranchIndustry("Retail");
+        device1.setOtp("123321");
+
+        Device device2 = new Device();
+        device2.setDeviceName("TestEGS2");
+        device2.setCity("Riyadh");
+        device2.setCitySubdiv("Test Dist.");
+        device2.setStreet("Test St.");
+        device2.setPlot("1234");
+        device2.setBuilding("1234");
+        device2.setPostal("12345");
+        device2.setBranchName("Riyad Branch 2");
+        device2.setBranchIndustry("Retail");
+        device2.setOtp("321123");
+
+        devices.add(device1);
+        devices.add(device2);
+
+        egsData.setDevices(devices);
+        Gson gson = new Gson();
+        assertThat(api.onboardEGS(gson.toJson(egsData)).toString(), containsString("Hello"));
     }
 
     @Test
     public void testGenerateInvoice() {
-        assertThat(api.generateInvoice("").toString(), containsString("Hello"));
+
+        InvoiceData invoiceData = new InvoiceData();
+        invoiceData.setEgsUuid("7b9cc231-0e14-4bff-938c-4603fe10c4bc");
+        invoiceData.setInvoiceRefNumber("INV-5");
+        invoiceData.setIssueDate("2023-01-01");
+        invoiceData.setIssueTime("01:40:40");
+
+        // Create and set the partyDetails object
+        PartyDetails partyDetails = new PartyDetails();
+        partyDetails.setPartyNameAr("شركة اختبار");
+        partyDetails.setPartyVat("300001111100003");
+
+        PartyAddId partyAddId = new PartyAddId();
+        partyAddId.setCrn(45463464);
+        partyDetails.setPartyAddId(partyAddId);
+
+        partyDetails.setCityAr("جدة");
+        partyDetails.setCitySubdivisionAr("حي الشرفية");
+        partyDetails.setStreetAr("شارع الاختبار");
+        partyDetails.setPlotIdentification("1234");
+        partyDetails.setBuilding("1234");
+        partyDetails.setPostalZone("12345");
+
+        invoiceData.setPartyDetails(partyDetails);
+
+        invoiceData.setDocType("388");
+        invoiceData.setInvType("standard");
+        invoiceData.setPaymentMethod(10);
+        invoiceData.setCurrency("SAR");
+        invoiceData.setTotalTax(142.0);
+        invoiceData.setHasAdvance(true);
+
+        // Create and set the advanceDetails object
+        AdvanceDetails advanceDetails = new AdvanceDetails();
+        advanceDetails.setAdvanceAmount(575.0);
+        advanceDetails.setTotalAmount(2875.0);
+
+        // Create and set the advanceInvoices list
+        AdvanceInvoice advanceInvoice = new AdvanceInvoice();
+        advanceInvoice.setTaxCategory("S");
+        advanceInvoice.setTaxPercentage(0.15);
+        advanceInvoice.setTaxableAmount(500.0);
+        advanceInvoice.setTaxAmount(75.0);
+
+        // Create and set the invoices list
+        Invoice invoice = new Invoice();
+        invoice.setId("INV-1");
+        invoice.setIssueDate("2022-12-10");
+        invoice.setIssueTime("12:28:17");
+
+        advanceInvoice.getInvoices().add(invoice);
+
+        advanceDetails.getAdvanceInvoices().add(advanceInvoice);
+
+        invoiceData.setAdvanceDetails(advanceDetails);
+
+        // Create and set the lineItems list
+        List<LineItem> lineItems = new ArrayList<>();
+        LineItem lineItem1 = new LineItem();
+        lineItem1.setNameAr("متحرك");
+        lineItem1.setQuantity(1.0);
+        lineItem1.setTaxCategory("S");
+        lineItem1.setTaxExclusivePrice(750.0);
+        lineItem1.setTaxPercentage(0.15);
+
+        LineItem lineItem2 = new LineItem();
+        lineItem2.setNameAr("حاسوب محمول");
+        lineItem2.setQuantity(1.0);
+        lineItem2.setTaxCategory("S");
+        lineItem2.setTaxExclusivePrice(1750.0);
+        lineItem2.setTaxPercentage(0.15);
+
+        lineItems.add(lineItem1);
+        lineItems.add(lineItem2);
+
+        invoiceData.setLineItems(lineItems);
+
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(invoiceData));
+        assertThat(api.generateInvoice(gson.toJson(invoiceData)).toString(), containsString("Hello"));
     }
 
     @Test
     public void testDoComplianceCheck() {
-        assertThat(api.doComplianceCheck("").toString(), containsString("Hello"));
+        assertThat(api.doComplianceCheck("7b9cc231-0e14-4bff-938c-4603fe10c4bc").toString(), containsString("Hello"));
     }
 
 }
